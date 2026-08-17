99fcf4c96a2dc04a65b5c839834c1e1484a64427
vassyusha <romanoffmiss@yandex.ru>
2026-08-17 14:20:07 +0300

    oks

--- 02/main.sh
+++ 02/main.sh
@@ -49,12 +49,4 @@ SPACE_ROOT_FREE = ${SPACE_ROOT_FREE} MB"
 
 echo "$OUTPUT"
 
-read -p "Записать данные в файл? (Y/N): " ANSWER
-
-if [[ "$ANSWER" == "Y" || "$ANSWER" == "y" ]]; then
-    FILENAME=$(date +"%d_%m_%y_%H_%M_%S.status")
-    echo "$OUTPUT" > "$FILENAME"
-    echo "Данные записаны в файл: $FILENAME"
-else
-    echo "Сохранение отменено."
-fi
+bash ./to_file.sh $OUTPUT
---
+++ 02/to_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+OUTPUT = $1
+
+read -p "Записать данные в файл? (Y/N): " ANSWER
+
+if [[ "$ANSWER" == "Y" || "$ANSWER" == "y" ]]; then
+    FILENAME=$(date +"%d_%m_%y_%H_%M_%S.status")
+    echo "$OUTPUT" > "$FILENAME"
+    echo "Данные записаны в файл: $FILENAME"
+else
+    echo "Сохранение отменено."
+fi
